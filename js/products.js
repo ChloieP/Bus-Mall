f8456315803c7ed75d48c6cceb0a28341d9c0e04
ChloieP <chloieparsons@gmail.com>
2019-04-02 05:42:04 -0700

    Roughed in CSS, JS, and HTML via class demo

---
+++ js/products.js
@@ -0,0 +1,12 @@
+// Starter code for function
+
+// What do I want to add the event handler on
+var container = document.getElementById('container');
+
+// What is the event handler
+function handleClick(event) {
+  console.log('Yahoo!');
+
+}
+//Add event handler to DOM
+container.addEventListener('click', handleClick);
