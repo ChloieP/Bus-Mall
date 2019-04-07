e37381199ba7f05331194ef1b9463c5acae7fd6d
ChloieP <chloieparsons@gmail.com>
2019-04-07 15:25:04 -0700

    Unified classes and ids between JS, CSS, HTML; establish global variablesand about three-fourths of the required functions as determined to this point

--- js/products.js
+++ js/products.js
@@ -1,23 +1,69 @@
+'use strict';
+
+// Global Array/Storage needed
+
+// Global Variables
+
+var productPhoto = document.getElementById('productPhoto');
+var PRODUCTS = {};
+var totalVotesOnPage = 0;
+
 // Starter code for function
 
-// What do I want to add the event handler on
-var container = document.getElementById('container');
+function Product(){
+  this.imgFilePath = imgFilePath;
+  this.name = name;
+  this.totalVotes = this.totalViews = 0;
+  this.HTMLid = HTMLid;
 
-var totalClicks = 0;
+  PRODUCTS[this.HTMLid] = this;
+}
+
+Product.prototype.getPercentClicked = function(){
+  return this.totalVotes / this.totalViews;
+};
+
+Product.prototype.render = function(parentId){
+  var parent = document.getElementById(parentId);
+
+  var img = document.createElement('img');
+  img.setAttribute('id', this.HTMLid);
+  img.setAttribute('src', this.imgFilePath);
+  img.setAttribute('class', 'product');
+
+  parent.appendChild(img);
+};
+
+function randomlySelectNewImage(){
+
+}
+
+function addCurrentSetOfImages(event){
+  event.target.id = 'dragon';
+  event.target.src = './img/dragon.jpg';
+}
+
+function displayResults(){
+
+}
 
 // What is the event handler
 function handleClick(event) {
-  var target = event.target;
-  if (target.className === 'product'){
-    event.target.id = 'dragon';
-    event.target.src = './img/dragon.jpg';
-    totalClicks++; //add limiter for clicks
+  if (event.target.className === 'product_photo'){
+    PRODUCTS[event.target.id].totalVotesOnPage++;
 
-    if(totalClicks === 25){  //click limiter if statement needs to be outside handleClick
-      container.removeEventListener('click'); //read about on MDN
-      totalClicks = 0;
+    //TO DO: If total clicks reached, stop listening
+    if(totalVotesOnPage === 25){ //click limiter if statement needs to be outside handleClick
+      productPhoto.removeEventListener('click'); //read about on MDN
+      totalVotesOnPage = 0;
+      displayResults();
+      return;
     }
+
+    randomlySelectNewImage();
+    addCurrentSetOfImages(event);
   }
 }
+
 //Add event handler to DOM
-container.addEventListener('click', handleClick);
+productPhoto.addEventListener('click', handleClick);
