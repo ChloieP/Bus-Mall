5648d964d8f270e1b683e93c63431d2f7de9fc50
ChloieP <chloieparsons@gmail.com>
2019-04-07 21:25:33 -0700

    Chart JS demo added to chartjs branch

--- js/products.js
+++ js/products.js
@@ -111,3 +111,33 @@ function votedImage(event) {
 // var bag = new Product ('./img/bag.jpg', 'bag', 'bag');
 // console.log(productArray[i]);
 
+var canvas = document.getElementById('myChart');
+var ctx = canvas.getContext('2d');
+ctx.width = 50;
+ctx.height = 50;
+
+var backgroundColors = [
+  'rgb(255, 99, 32)',
+  'rgb(20, 40, 230)',
+  'rgb(10, 240, 6)'
+];
+
+// Chart JS
+
+var datasetItem = {
+  data: [10, 20, 40],
+  backgroundColor: backgroundColors
+}
+
+var data = {
+  labels: ['red', 'blue', 'green'],
+  datasets: [datasetItem]
+};
+
+var pieChartConfig = {
+  type: 'doughnut',
+  data: data,
+};
+
+var pieChart = new Chart(ctx, pieChartConfig);
+
