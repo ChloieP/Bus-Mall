83137ae55a619b5f193d77ac1bf61daf5e0de56e
ChloieP <chloieparsons@gmail.com>
2019-04-07 18:31:49 -0700

    Got array by numeric value, setup math for random picture selection, moving on to next lab  but will continue to work on this

--- js/products.js
+++ js/products.js
@@ -1,16 +1,42 @@
 'use strict';
 
-// Global Array/Storage needed
+// Global Scope Variables & Functions
 
-// Global Variables
-
-var productPhoto = document.getElementById('productPhoto');
-var PRODUCTS = {};
+var productImg = document.getElementById('productImg');
 var totalVotesOnPage = 0;
+var maxImages = 19;
+var minImages = 0;
+var PRODUCTS = {};
+var productArray = [
+  ['./img/bag.jpg', 'bag', 'bag'],
+  ['./img/banana.jpg', 'banana', 'banana'],
+  ['./img/bathroom.jpg', 'bathroom', 'bathroom'],
+  ['./img/boots.jpg', 'boots', 'boots'],
+  ['./img/breakfast.jpg','breakfast', 'breakfast'],
+  ['./img/bubblegum.jpg', 'bubblegum', 'bubblegum'],
+  ['./img/chair.jpg', 'chair ', 'chair'],
+  ['./img/cthulhu.jpg', 'cthulhu', 'cthulhu'],
+  ['./img/dog-duck.jpg', 'dog-duc', 'dog-duc'],
+  ['./img/dragon.jpg', 'dragon', 'dragon'],
+  ['./img/pen.jpg', 'pen', 'pen'],
+  ['./img/pet-sweep.jpg', 'pet-sweep', 'pet-sweep'],
+  ['./img/scissors.jpg', 'scissors', 'scissors'],
+  ['./img/shark.jpg', 'shark', 'shark'],
+  ['./img/sweep.png', 'sweep', 'sweep'],
+  ['./img/tauntaun.jpg', 'tauntaun', 'tauntaun'],
+  ['./img/unicorn.jpg', 'unicorn', 'unicorn'],
+  ['./img/usb.gif', 'usb', 'usb'],
+  ['./img/water-can.jpg', 'water-can', 'water-can'],
+  ['./img/wine-glass.jpg', 'wine-glass', 'wine-glass'],
+];
 
-// Starter code for function
+//https://stackoverflow.com/questions/38824349/how-to-convert-an-object-to-an-array-of-key-value-pairs-in-javascript
 
-function Product(){
+var PRODUCTS_ARRAY = Object.keys(productArray).map(function(key){
+  return [Number(key), productArray[key]];
+});
+
+function Product(imgFilePath, name, HTMLid){
   this.imgFilePath = imgFilePath;
   this.name = name;
   this.totalVotes = this.totalViews = 0;
@@ -25,8 +51,8 @@ Product.prototype.getPercentClicked = function(){
 
 Product.prototype.render = function(parentId){
   var parent = document.getElementById(parentId);
-
   var img = document.createElement('img');
+
   img.setAttribute('id', this.HTMLid);
   img.setAttribute('src', this.imgFilePath);
   img.setAttribute('class', 'product');
@@ -34,36 +60,54 @@ Product.prototype.render = function(parentId){
   parent.appendChild(img);
 };
 
-function randomlySelectNewImage(){
+// Renders products, puts them in the DOM, gets totalViews, and adds eventListener to images
 
+function newProduct(){
+  for (var i = 0; i < productArray.length; i++ ){
+    new Product(productArray[i][0], productArray[i][1], productArray[i][2]);
+  }
 }
 
-function addCurrentSetOfImages(event){
-  event.target.id = 'dragon';
-  event.target.src = './img/dragon.jpg';
+function randomlySelectNewImage(minImages, maxImages){
+  return Math.random() * (maxImages - minImages) + minImages;
 }
 
-function displayResults(){
+// function renderImages() {
+//   for (var i = 0; i < 3; i++) {
 
-}
+//   }
+// }
 
-// What is the event handler
-function handleClick(event) {
+var listResults = function (){
+  document.createElement('resultsList');
+
+  // PRODUCTS_ARRAY.sort(function(a, b)) {
+
+};
+
+// Event Handler
+
+function votedImage(event) {
   if (event.target.className === 'product_photo'){
     PRODUCTS[event.target.id].totalVotesOnPage++;
 
-    //TO DO: If total clicks reached, stop listening
-    if(totalVotesOnPage === 25){ //click limiter if statement needs to be outside handleClick
-      productPhoto.removeEventListener('click'); //read about on MDN
+    if(totalVotesOnPage === 25){ //click limiter if statement needs to be outside votedImage
+      productImg.removeEventListener('click', function votedImage(event){});
       totalVotesOnPage = 0;
-      displayResults();
+      listResults();
       return;
     }
 
     randomlySelectNewImage();
     addCurrentSetOfImages(event);
+
   }
 }
 
 //Add event handler to DOM
-productPhoto.addEventListener('click', handleClick);
+// productImg.addEventListener('click', handleClick);
+//entry point
+
+// var bag = new Product ('./img/bag.jpg', 'bag', 'bag');
+// console.log(productArray[i]);
+
